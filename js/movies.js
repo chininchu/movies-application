56328d21f3b318bc98aac6ee592808fef7865905
Asia Autrey <asia.autrey0@gmail.com>
2023-04-14 15:05:38 -0500

    complete

--- js/movies.js
+++ js/movies.js
@@ -1,36 +1,44 @@
-// $(document).ready(function () {
 
 setTimeout(function () {
     $('#loading').hide()
     myMovies()
-}, 2000)
-// addMovies()
-setTimeout(() =>
-    $("#addBtn").click(function (event) {
-        event.preventDefault();
-        let title = event.target.title
-        addMovies(title)
-        myMovies()
-    }), 1500)
+}, 1500)
 
-setTimeout(() =>
-    $(".deleteBtn").click(function (event) {
+
+setTimeout(() => {
+    $("#addBtn").click(function (event) {
+        console.log('click')
         event.preventDefault();
+        let titleinput = document.getElementById("title").value;
+        addMovies(titleinput)
+        myMovies()
+        setTimeout(function (){
+            location.reload()
+        }, 2000)
+    })
+}, 2000)
+
+setTimeout(() => $(".deleteBtn").click(function (event) {
+        console.log('click on delete btn')
+        // event.preventDefault();
         let id = event.target.id
         deleteMovies(id)
-        myMovies()
-    }), 1500)
-
-setTimeout(() =>
-    $(".editBtn").click(function (event) {
-        event.preventDefault();
-        let id = event.target.id
-        editPost(id)
-        myMovies()
-    }), 1500)
+        location.reload()
+    }), 2000)
 
 
-// Currently working on implementing the Grid system
+setTimeout(() => {
+console.log("editBtn")
+$(".editBtn").click(function (event) {
+    // event.preventDefault();
+    let id = event.target.id;
+    editMovies(id);
+    setTimeout(function (){
+        location.reload()
+    }, 2000)
+})
+}, 2000)
+
 
 function myMovies() {
     let html = ''
@@ -38,26 +46,23 @@ function myMovies() {
         .then(response => response.json())
         .then(movieData => {
             movieData.map(data => {
-
-                html += `<div class="d-flex" style="height: 200px;">`
+                html += `<div class="d-flex">`
                 html += `<div>`
+                html += `<p className='movie-para'><img src="${data.imgUrl}" id='movieimg' alt="poster">`
                 html += `<span class="w-25 "> ${data.id} </span>`
-                html += `<h3 class="w-25 "> ${data.title} </h3>`
+                html += `<h3 class="w-25 "> ${data.title} </h3> </p>`
                 html += `<h6 class="w-25 "> ${data.rating} </h6>`
                 html += `<h6 class="w-25 "> ${data.genre} </h6>`
-                // html += `<h6 class="w-25 "> ${data.imgUrl} </h6>`
                 html += `</div>`
-                html += ` <form class="" id="editForm">`
-                html += ` <label  class="" for="edittitle"></label>`
-                html += ` <input  class="" name="edittile" id="edittitle" type="text" placeholder="title">`
-                html += ` <label  class="" for="editgenre"></label>`
-                html += ` <input  class="" name="editgenre" id="editgenre" type="text" placeholder="genre">`
-                html += ` <label  class="" for="editrating"></label>`
-                html += ` <input  class="" name="editrating" id="editrating" type="text" placeholder="rating">`
-                html += ` <label  class="" for="editimage"></label>`
-                html += ` <input  class="" name="editimage" id="editimage" type="text" placeholder="image here">`
-                html += `  <button  id="${data.id}" type="button" class="editBtn btn-primary">Edit</button>`
-                html += `  <button id="${data.id}" type="button" class="deleteBtn btn-primary ">Delete</button>`
+                html += ` <form class="editForm">`
+                html += ` <label for="edittitle"></label>`
+                html += ` <input  name="edittile" class="edittitle" type="text" placeholder="title">`
+                html += ` <label  for="editgenre"></label>`
+                html += ` <input  name="editgenre" class="editgenre" type="text" placeholder="genre">`
+                html += ` <label  for="editrating"></label>`
+                html += ` <input  name="editrating" class="editrating" type="text" placeholder="rating">`
+                html += `  <button id="${data.id}" type="button" class="editBtn primary">Edit</button>`
+                html += `  <button id="${data.id}" type="button" class="deleteBtn primary ">Delete</button>`
                 html += ` </form>`
                 html += `</div>`
 
@@ -66,63 +71,41 @@ function myMovies() {
             $('.movie-title').html(html)
         })
         .catch(error => console.log(error))
-
 }
 
-
-function postMovies() {
-    fetch('http://localhost:3000/movies', {
-        // establish the method, GET is the default method.
-        method: 'POST',
-        headers: {
-            // the server needs to know what format we are sending data in.
-            'Content-Type': 'application/json'
-        },
-        // convert JS object to JSON object
-        body: JSON.stringify({
-            title: $('#title').val(),
-            genre: $('#genre').val(),
-            rating: $('#rating').val(),
-            imgUrl: $('#image').val()
-
-        })
-    })
-        .then(res => res.json())
-        .then(data => console.log(data))
-        .catch(error => console.log(error));
-}
 
 function deleteMovies(id) {
     console.log('test')
     fetch(`http://localhost:3000/movies/${id}`, {
-        method: 'DELETE',
+        method:'DELETE',
 
     })
         .then(res => res.json())
         .then(data => console.log(data))
         .catch(error => console.log(error));
-
-    myMovies();
 }
 
 
-function editPost(title) {
-    return fetch(`http://localhost:3000/movies/${title}`, {
+function editMovies(id) {
+    fetch(`http://localhost:3000/movies/${id}`, {
         method: 'PATCH',
         headers: {
             'Content-Type': 'application/json'
         },
         body: JSON.stringify({
-            title: $('#edittitle').val(),
-            genre: $('#editgenre').val(),
-            rating: $('#editrating').val(),
-            imgUrl: $('#editimage').val(),
+            title: $('.edittitle').val(),
+            genre: $('.editgenre').val(),
+            rating: $('.editrating').val(),
         })
     })
         .then(resp => resp.json())
         .then(data => data)
         .catch(error => console.error(error))
+
 }
+
+
+
 
 function addMovies(title) {
     const options = {
@@ -135,34 +118,24 @@ function addMovies(title) {
     fetch(`https://online-movie-database.p.rapidapi.com/title/find?q=${title}`, options)
         .then(response => response.json())
         .then(response => {
-            const movies = response.results;
-            movies.map(movie => {
-                // const movieTitle= movie[0].title
-                // console.log(movieTitle)
-                console.log(movie.image)
-            })
-            // fetch('http://localhost:3000/movies', {
-            //     // establish the method, GET is the default method.
-            //     method: 'POST',
-            //     headers: {
-            //         // the server needs to know what format we are sending data n.
-            //         'Content-Type': 'application/json'
-            //     },
-            //     // convert JS object to JSON object
-            //     body: JSON.stringify({
-            //         title: 'results[0].title',
-            //         genre: $('#genre').val(),
-            //         rating: $('#rating').val(),
-            //         imgUrl: 'results[0].image.url'
-            //
-            //     })
-            //
-            // })
+            let movieTitle = response.results[0].title
+            let movieImage = response.results[0].image.url
+            fetch('http://localhost:3000/movies', {
+                // establish the method, GET is the default method.
+                method: 'POST',
+                headers: {
+                    // the server needs to know what format we are sending data n.
+                    'Content-Type': 'application/json'
+                },
+                // convert JS object to JSON object
+                body: JSON.stringify({
+                    title: movieTitle,
+                    genre: $('#genre').val(),
+                    rating: $('#rating').val(),
+                    imgUrl: movieImage
+
+                })
+            }).then(response => myMovies())
+
         })
 }
-
-addMovies('shrek')
-
-
-
-
